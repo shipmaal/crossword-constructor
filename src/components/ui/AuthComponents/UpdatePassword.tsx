169614d7b9a18a9ebb863e1d8e938a8785af390f
shipmaal <100368040+shipmaal@users.noreply.github.com>
2024-02-24 16:04:53 -0500

    Update password component, migrate database, and update tsconfig

--- src/components/ui/AuthComponents/UpdatePassword.tsx
+++ src/components/ui/AuthComponents/UpdatePassword.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 'use client';
 
 import Button from '@/components/ui/Button/Button';
--- src/migrate.ts
+++ src/migrate.ts
@@ -3,6 +3,7 @@ import { migrate } from 'drizzle-orm/postgres-js/migrator';
 import { db, client } from './db';
 
 async function main() {
+    await migrate(db, { migrationsFolder: './drizzle' });
 
     // Don't forget to close the connection, otherwise the script will hang
     await client.end();
